14f5c580e1343b72de24b66f78b5043036fe8928
baturox <baturox@gmail.com>
2018-08-13 23:09:51 +0300

    Entity Classes Changed

---
+++ app/Http/Entity/CustomerAddressEntity.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Entity;
+
+class CustomerAddressEntity
+{
+    public $id;
+    public $userId;
+    public $postCode;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param mixed $userId
+     */
+    public function setUserId($userId): void
+    {
+        $this->userId = $userId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPostCode()
+    {
+        return $this->postCode;
+    }
+
+    /**
+     * @param mixed $postCode
+     */
+    public function setPostCode($postCode): void
+    {
+        $this->postCode = $postCode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdress()
+    {
+        return $this->adress;
+    }
+
+    /**
+     * @param mixed $adress
+     */
+    public function setAdress($adress): void
+    {
+        $this->adress = $adress;
+    }
+    public $adress;
+
+
+}
