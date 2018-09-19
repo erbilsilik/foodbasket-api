0ebef26ad4f6cb299dbab8164772c77ec4360293
baturox <baturox@gmail.com>
2018-09-19 15:32:03 +0300

    Namespaces and Assets Deleted 3

---
+++ app/Http/Controllers/RestaurantController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Restaurant;
+use Illuminate\Http\Request;
+use App\Http\Manager\RestaurantManager;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redis;
+
+class RestaurantController extends Controller
+{
+    private $restaurantManager;
+
+    /**
+     * RestaurantController constructor.
+     */
+    public function __construct()
+    {
+        $this->restaurantManager = new RestaurantManager();
+    }
+
+    // GENERAL ENDPOINTS //
+
+    public function searchRestaurants(Request $request)
+    {
+        $postCode = $request->get('postcode');
+        return response()
+            ->json($this->restaurantManager->searchRestaurantsByPostCode($postCode));
+    }
+
+    // ENDPOINTS FOR OWNERS //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()
+            ->json($this->restaurantManager->getRestaurantList());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return $this->restaurantManager->addRestaurant($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return response()
+            ->json($this->restaurantManager->getRestaurantById($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return response()
+            ->json($this->restaurantManager->updateRestaurant($id, $request->all()));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $this->restaurantManager->deleteRestaurant($id);
+
+        return response(204);
+    }
+}
